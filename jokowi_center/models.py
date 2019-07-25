6304dd437a2d46e18b78e727f15ec6bb6f05b153
agatharach <agatha@alterra.id>
2019-07-25 19:11:35 +0700

    Halaman ketiga plus input

--- jokowi_center/models.py
+++ jokowi_center/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Masukan (models.Model):
+    nama_lengkap = models.CharField(max_length=225)
+    alamat_email = models.CharField(max_length=225)
+    judul = models.CharField(max_length=100)
+    isi_pesan = models.TextField()
+    def __str__(self):
+        return self.nama_lengkap
+
--- jokowi_center/urls.py
+++ jokowi_center/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-path('',views.jkw_center_main,name="jkw_center_main")
-
+    # path('',views.jkw_center_main,name="jkw_center_main")
+    path('',views.listmasukan, name='halaman_feedback')
 ]
