103e7ad2e468111cda0e3c900b66166964e5c562
Ahmad Rizqita <rizqita@alterra.id>
2019-07-25 18:51:22 +0700

    3rd commit , social media , index page, add some on base

--- jokowi_center/models.py
+++ jokowi_center/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Blog(models.Model):
+    judul=models.CharField(max_length=200)
+    gambar=models.CharField(max_length=200)
+    isi=models.TextField()
+    date =models.DateTimeField(auto_now=True)
+    def __str__(self):
+        return self.judul
--- jokowi_center/urls.py
+++ jokowi_center/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-path('',views.jkw_center_main,name="jkw_center_main")
+path('',views.jkw_center_main,name="jkw_center_main"),
+path('socialmedia',views.jkw_social_media,name="jkw_social_media"),
 
 ]
