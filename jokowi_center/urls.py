0e86e43d50c480b8ec88e6bb7a40ead179b8348b
rizqita <52493615+rizqita@users.noreply.github.com>
2019-07-26 08:16:36 +0700

    add file via upload
    modified views.py , deleting some typo

--- jokowi_center/urls.py
+++ jokowi_center/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-path('',views.jkw_center_main,name="jkw_center_main"),
-path('socialmedia',views.jkw_social_media,name="jkw_social_media"),
-
+    path('',views.jkw_center_main,name="jkw_center_main"),
+    path('hubungikami/',views.listmasukan, name='hubungikami'),
+    path('socialmedia/',views.jkw_social_media,name="jkw_social_media"),
 ]
--- jokowi_center/views.py
+++ jokowi_center/views.py
@@ -1,10 +1,27 @@
-from django.shortcuts import render
-from .models import Blog
+from django.shortcuts import render,redirect
+from .models import Masukan, Blog
+from .forms import Inputfeedback 
 
 # Create your views here.
+# def jkw_center_main(request):
+#     return render(request,'Hubungikami.html',{})
+def listmasukan(request):
+    if request.method == 'POST':
+        form = Inputfeedback(request.POST)
+        if form.is_valid():
+            post=form.save(commit=False)
+            post.save()
+            return redirect('/')
+    else:
+        form = Inputfeedback()
+    return render(request,'Hubungikami.html',{'form':form})
+def masukan_view(request):
+    masukan = list_masukan.all()
+    return render(request,'index.html',{'masukan':masukan})   
 def jkw_center_main(request):
     blog = Blog.objects.all()
     return render(request,'index.html',{'blog':blog})
+
 def jkw_social_media(request):
     return render(request,'socialmedia.html')
 # def jkw_blog_post(request):
